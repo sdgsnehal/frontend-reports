a3757eb2792062ca4d1ac1a3c1e661627d4d1a3f
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-01-15 12:35:45 +0530

    feat(package.json): add react-datepicker library for date picking functionality feat(main.tsx): import and use react-datepicker component for date picking feat(Order.tsx): add date range dropdown and date pickers using react-datepicker for filtering orders by date feat(Constants.ts): create DateRange constant for date filtering options in Order component

--- src/pages/order/Order.tsx
+++ src/pages/order/Order.tsx
@@ -1,11 +1,32 @@
 import GlobalTable from "../../components/Table/Global/GlobalTable";
 import Layout from "../Layout/Layout";
 import { OrderData } from "../../components/Table/data";
+import Dropdown from "../../components/Dropdown/Dropdown";
+import { DateRange } from "../../utils/Constants";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
 
 const Order = () => {
+  const [startDate, setStartDate] = useState(new Date());
+
   return (
     <Layout>
       <div>
+        <div>
+          <div className="flex flex-row gap-2 items-center">
+            <Dropdown Items={DateRange} Light={true} />
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              className="p-[5px] rounded-md"
+            />
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              className="p-[5px] rounded-md"
+            />
+          </div>
+        </div>
         <GlobalTable data={OrderData} />
       </div>
     </Layout>
---
+++ src/utils/Constants.ts
@@ -0,0 +1,5 @@
+export const DateRange = [
+  { id: 1, name: "Last 30 Days" },
+  { id: 2, name: "This Month" },
+  { id: 3, name: "Last Month" },
+];
