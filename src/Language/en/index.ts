03f394d86104038acae18fa4459ce84d02df727e
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-01-23 12:45:54 +0530

    feat(package.json): add react-hook-form dependency to enable form validation and management in the login page feat(Language): add English language messages for form validations in the login page feat(pages/login): implement form validation and submission handling using react-hook-form in the login page

---
+++ src/Language/en/index.ts
@@ -0,0 +1,5 @@
+export const messages = {
+  VALIDATIONS: {
+    INVALID_EMAIL_ADDRESS: "Please enter valid email address",
+  },
+};
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,34 @@
+import { Controller, useForm } from "react-hook-form";
+import secureLocalStorage from "react-secure-storage";
+import { messages } from "../../Language/en";
+type FormValues = {
+  email: string;
+  password: string;
+  remember: boolean;
+};
+const getDefaultValues = () => {
+  const email = secureLocalStorage.getItem("remember_me_email") as string;
+  const password = secureLocalStorage.getItem("remember_me_password") as string;
+  const remember = secureLocalStorage.getItem("remember_me_flag") as boolean;
 
-
+  return {
+    email: email ?? "",
+    password: password ?? "",
+    remember: remember ?? false,
+  };
+};
 const Login = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<FormValues>({
+    defaultValues: getDefaultValues(),
+  });
+  const onSubmit = async (values: FormValues) => {
+    const { email, password } = values;
+    console.log(email, password);
+  };
   return (
     <div className="flex flex-col justify-center items-center w-full">
       <header className="bg-[#536c8c] flex items-center justify-center w-full">
@@ -10,7 +38,10 @@ const Login = () => {
         Welcome to Reports
       </h3>
 
-      <form className="bg-white p-4 w-full md:w-1/3 rounded-lg shadow-md">
+      <form
+        className="bg-white p-4 w-full md:w-1/3 rounded-lg shadow-md"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <p className="text-lg font-semibold text-center text-black">
           Sign in to your account
         </p>
@@ -46,20 +77,45 @@ const Login = () => {
           <div className="h-px w-full bg-gray-500"></div>
         </div>
         <div className="relative mt-4">
-          <input
-            type="email"
-            placeholder="Enter email"
-            className="w-full bg-white p-4 pr-12 text-sm border border-gray-300 rounded-lg shadow-sm outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+          <Controller
+            control={control}
+            rules={{
+              required: messages.VALIDATIONS.INVALID_EMAIL_ADDRESS,
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: messages.VALIDATIONS.INVALID_EMAIL_ADDRESS,
+              },
+            }}
+            render={({ field: { onChange, value } }) => (
+              <input
+                type="email"
+                placeholder="Enter email"
+                value={value}
+                onChange={onChange}
+                required
+                className="w-full bg-white p-4 pr-12 text-sm border border-gray-300 rounded-lg shadow-sm outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            )}
+            name="email"
           />
         </div>
         <div className="relative mt-4">
-          <input
-            type="password"
-            placeholder="Enter password"
-            className="w-full bg-white p-4 text-sm border border-gray-300 rounded-lg shadow-sm outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+          <Controller
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <input
+                type="password"
+                placeholder="Enter password"
+                value={value}
+                required
+                onChange={onChange}
+                className="w-full bg-white p-4 text-sm border border-gray-300 rounded-lg shadow-sm outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            )}
+            name="password"
           />
         </div>
-       
+
         <button
           type="submit"
           className="w-full mt-4 bg-indigo-600 text-white py-3 px-5 text-sm font-medium rounded-lg uppercase hover:bg-indigo-700 focus:ring-4 focus:ring-indigo-500"
