49730e5f5f6e08706c0d6b3bf6ad0ef800fe2c11
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-01-23 10:37:50 +0530

    chore(.env): update VITE_API_ENDPOINT to VITE_API_HOST for consistency feat(package.json): add axios package for making HTTP requests to the API feat(api-client.ts): create API client module with Axios for making HTTP requests to the API

---
+++ src/lib/api-client.ts
@@ -0,0 +1,40 @@
+import Axios, { Method } from "axios";
+
+const axios = Axios.create({
+  baseURL: import.meta.env.VITE_API_HOST,
+  withCredentials: true,
+  withXSRFToken: true,
+});
+
+// A function that calls '/api/csrf-cookie' to set the CSRF cookies. The
+// default is 'sanctum/csrf-cookie' but you can configure it to be anything.
+const setCSRFToken = () => {
+  return axios.get("/api/csrf-cookie");
+};
+
+axios.interceptors.request.use((config) => {
+  // If http method is `post | put | delete` and XSRF-TOKEN cookie is
+  // not present, call '/sanctum/csrf-cookie' to set CSRF token, then
+  // proceed with the initial response
+  if (
+    config.method === "post" ||
+    config.method === "put" ||
+    config.method === "delete"
+  ) {
+    return setCSRFToken().then(() => config);
+  }
+
+  return config;
+});
+
+export default async function API(method: Method = "get", url = "", data = {}) {
+  try {
+    const response = await axios[method](url, data);
+    if (!response) {
+      throw false;
+    }
+    return response;
+  } catch (error: any) {
+    throw error.response;
+  }
+}
