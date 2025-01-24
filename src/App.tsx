b698cb340c13b728b8b123aeb5d0aa67f2d96b92
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-01-24 12:35:29 +0530

    feat(package.json): add "@reduxjs/toolkit" and "react-redux" dependencies to enable Redux state management in the application style(App.tsx): remove unnecessary whitespace to improve code readability feat(main.tsx): integrate Redux store using Provider component to provide Redux store to the application feat(store.ts): create Redux store using "@reduxjs/toolkit" to manage application state

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
         <AppRoutes />
       </BrowserRouter>
     </QueryClientProvider>
+     
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import "react-datepicker/dist/react-datepicker.css";
 import "./index.css";
 import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>
 );
