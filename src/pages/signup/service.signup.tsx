08bc72b463f8ef8436dd2d0a2772ae438a3358a4
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-01-27 12:46:22 +0530

    feat(service.signup.tsx): add a new signup service to handle user registration functionality

---
+++ src/pages/signup/service.signup.tsx
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import API from "../../lib/api-client";
+import secureLocalStorage from "react-secure-storage";
+type Signup = {
+  userName: string;
+  fullName: string;
+  email: string;
+  password: string;
+};
+export function useSignup() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (values: Signup) =>
+      API("post", "/api/v1/users/register", values),
+    onSuccess: (data) => {
+      const user = { data, isLoggedIn: true };
+      queryClient.setQueryData(["auth"], user);
+      secureLocalStorage.setItem("user", user);
+      navigate("/dashboard");
+    },
+  });
+}
