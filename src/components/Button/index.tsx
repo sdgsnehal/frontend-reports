16f4a889d355bafe98ce2d95c6e6d46bf56be59c
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-02-14 11:26:57 +0530

    style(Button): improve ButtonElement component structure for better readability and maintainability feat(Global): add loading state management using useLoading hook for better user experience feat(Global): add Loader component to display loading state visually feat(Global): add button to trigger fetching reports based on selected date range

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,21 @@
 import { Button } from "@headlessui/react";
-
-export default function ButtonElement() {
+interface ButtonElementProps {
+  text: string | null | undefined;
+  disabled?: boolean;
+  onClick?: () => void;
+}
+export default function ButtonElement({
+  text,
+  onClick,
+  disabled,
+}: ButtonElementProps) {
   return (
-    <Button className="block w-full items-center gap-2 rounded-md bg-gray-700 py-2 px-4 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white mt-3">
-      Login
+    <Button
+      className="block  items-center gap-2 rounded-md bg-gray-700 py-2 px-4 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white mt-3"
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {text}
     </Button>
   );
 }
--- src/pages/Global/Global.tsx
+++ src/pages/Global/Global.tsx
@@ -9,14 +9,24 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 import { GlobalData } from "../../store/getGlobalSlice";
 import DatePicker from "react-datepicker";
+import ButtonElement from "../../components/Button";
+import { useLoading } from "../../components/loader/loadingContext";
+import Loader from "../../components/loader/loader";
 
 const Global = () => {
   const [startDate, setStartDate] = useState<Date | null>(new Date());
   const [endDate, setEndDate] = useState<Date | null>(new Date());
   const globalDataTable = useSelector((state: RootState) => state.globalData);
   const lastGlobalDataRef = useRef(globalDataTable);
+  const { loading, setLoading } = useLoading();
   const dispatch = useDispatch();
-  const { data: global, refetch } = useGlobal({
+  const {
+    data: global,
+    refetch,
+    isLoading,
+    isSuccess,
+    isError,
+  } = useGlobal({
     startDate: startDate?.toISOString() ?? "",
     endDate: endDate?.toISOString() ?? "",
   });
@@ -29,22 +39,31 @@ const Global = () => {
       lastGlobalDataRef.current = global;
     }
   }, [global, dispatch]);
-
+  if (isLoading) {
+    setLoading(isLoading);
+  }
+  if (isSuccess) {
+    setLoading(false);
+  }
+  if (isError) {
+    setLoading(false);
+  }
   const handleDropdownChange = (dates: [Date | null, Date | null]) => {
     if (Array.isArray(dates)) {
       const [start, end] = dates;
       setStartDate(start);
       setEndDate(end || null);
     }
+  };
+  const handleGetReports = () => {
     if (startDate !== null && endDate !== null) {
       refetch();
     }
   };
-
   return (
     <Layout>
       <div className="w-full bg-white/50  border-gray-500 rounded-xl flex flex-col p-1 mt-4">
-        <div className="px-4 py-2">
+        <div className="px-4 py-2 flex justify-items-start gap-2 items-center  ">
           <div className="text-sm text-black font-semibold py-2 px-4">
             SELECT DATE RANGE
           </div>
@@ -56,9 +75,10 @@ const Global = () => {
               endDate={endDate}
               selectsRange
               showIcon
-              className="p-[5px] rounded-md bg-white text-black text-sm font-semibold"
+              className="p-[5px] py-2 rounded-md bg-white text-black text-sm font-semibold"
             />
           </div>
+          <ButtonElement text="Get Report" onClick={handleGetReports} />
         </div>
         <GlobalTable data={globalDataTable} />
       </div>
@@ -74,6 +94,11 @@ const Global = () => {
           {/* <Linechart title="Last 6 Months" legend="right" /> */}
         </div>
       </div>
+      {loading && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <Loader />
+        </div>
+      )}
     </Layout>
   );
 };
